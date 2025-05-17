69f8e2cd863dd3f4316d88184cdd0419817d4447
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-17 23:57:03 +0600

    Problem solved : if else condition - 3

--- 15-if-else-condition.js
+++ 15-if-else-condition.js
@@ -18,15 +18,26 @@ function applyDiscount(totalPrice, discount, isPercentage = true) {
  
 //  2
 
-const age = 21;
+// const age = 21;
 
-if(age < 12){
-    console.log('Free of cost any food');
-}
-else if(age > 60){
-    console.log('They will get 60% discout!');
-}
-else{
-    console.log('They have to pay full price!');
-}
+// if(age < 12){
+//     console.log('Free of cost any food');
+// }
+// else if(age > 60){
+//     console.log('They will get 60% discout!');
+// }
+// else{
+//     console.log('They have to pay full price!');
+// }
 
+
+// 3 
+
+const accountBalance = 1000;
+
+if(accountBalance <= 1000){
+    console.log('Deposite kor');
+}
+else if(accountBalance > 1000 && accountBalance <= 5000){
+    console.log('Enjoy your life!');
+}

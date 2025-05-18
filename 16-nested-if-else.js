0fdf370906f7af0fe6f75763eb6454a18ae0df8c
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-18 15:38:16 +0600

    Problem solved : Nested if else - 1

---
+++ 16-nested-if-else.js
@@ -0,0 +1,20 @@
+// 1 
+
+const isInvited = true;
+const pocketAmount = 500;
+
+// if(isInvited){
+
+//     if(pocketAmount > 1000){
+//         console.log('Buy a Airpod for Friend');
+//     }
+//     else{
+//         console.log('Teke nai ami aise aida e jothesto');
+//     }
+// }
+// else{
+//     console.log('Break up with you!');
+// }
+
+
+

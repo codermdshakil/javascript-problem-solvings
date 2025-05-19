bbd250c36155731712fc121f734012be55e6273d
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-19 18:12:47 +0600

    Problem solved : Logical Not Operator - 1

---
+++ 17-logical-not-operator.js
@@ -0,0 +1,14 @@
+
+// JavaScript Logical Not Operator Practice
+
+// 1 
+// const totalPrice = 3001;
+// let cashBack = 0;
+// totalPrice > 3000 ? cashBack = 500 : cashBack = 0;
+
+// console.log(cashBack); // cashBack 500 
+
+
+
+
+

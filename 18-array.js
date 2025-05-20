aa8fb0a932a1773e2db3ed383c569bbaf5739fd4
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-20 17:07:50 +0600

    Problem solved : Array - 3

--- 18-array.js
+++ 18-array.js
@@ -8,3 +8,10 @@ const numsArr = [71, 72,73,74,75,76,77,78,79];
 // 2 
 
 const foods = ['apple', 'banana', 'orange', 'mango', 'guava'];
+
+
+// 3 
+
+const grosaryShop = ['alo', 'peyaj', 'begun', 'mula', 'gajor', 'tomato', 'deros', 'lau', 'sosa', 'potol'];
+// console.log(grosaryShop.length); // 10 
+

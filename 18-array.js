873d0071176e9b2dbf3b620bf536e2602740ab60
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-20 17:10:05 +0600

    Problem solved : Array - 4

--- 18-array.js
+++ 18-array.js
@@ -15,3 +15,8 @@ const foods = ['apple', 'banana', 'orange', 'mango', 'guava'];
 const grosaryShop = ['alo', 'peyaj', 'begun', 'mula', 'gajor', 'tomato', 'deros', 'lau', 'sosa', 'potol'];
 // console.log(grosaryShop.length); // 10 
 
+// 4 
+const favourateMovie = ['The Pursuit of Happyness', 'Good Will Hunting', 'The Social Network', 'A Beautiful Mind', 'Dead Poets Society'];
+
+// console.log(favourateMovie);
+

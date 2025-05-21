54cd1db4d7b623261b26f43499e824851aeb2e8c
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-21 16:45:30 +0600

    Problem solved : Array index - 3

--- 19-array-index.js
+++ 19-array-index.js
@@ -13,3 +13,8 @@ const favourateBooks = ['bela foraber age', 'jobab', 'eat that frog', 'atomic ha
 // console.log(favourateBooks[5]); // Rich dad poor dad 
 
 
+// 3 
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+numbers[6] = 30;
+

1fac2536a82b22f94d252e8becf2584353545015
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-21 16:42:27 +0600

    Problem solved : Array index - 2

--- 19-array-index.js
+++ 19-array-index.js
@@ -5,3 +5,11 @@ const friends = ['noyon', 'nadim', 'asraf', 'mahfuj', 'samiul'];
 
 // console.log(friends[3]); // mahfuj 
 
+
+// 2 
+
+const favourateBooks = ['bela foraber age', 'jobab', 'eat that frog', 'atomic habit', 'programming funadamental', 'Rich dad poor dad', 'The power of Now'];
+
+// console.log(favourateBooks[5]); // Rich dad poor dad 
+
+

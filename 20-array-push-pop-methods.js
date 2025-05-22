6f6cce297bddb760715e9b1c2977f4f64387252c
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-22 13:13:54 +0600

    Problem solved : Array push, pop, shift, unshift methods - 2

--- 20-array-push-pop-methods.js
+++ 20-array-push-pop-methods.js
@@ -6,3 +6,9 @@ const nums = [10,20,30,40,50];
 nums.push(60);
 // console.log(nums); // [ 10, 20, 30, 40, 50, 60 ]
 
+// 2
+const friends = ['Sojib', 'Sagor', 'Sakib', 'Sohel'];
+friends.push('Sumon');
+// console.log(friends); // [ 'Sojib', 'Sagor', 'Sakib', 'Sohel', 'Sumon' ]
+
+

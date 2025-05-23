89ec1bffa77a8ad9d3437eb01fea784da3ba5591
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-23 14:57:02 +0600

    Problme solved: Include and indexOf - 1

---
+++ 21-include-indexof.js
@@ -0,0 +1,12 @@
+
+// JavaScript Array Include and Indexof practice problem
+
+// 1. Include VS indexof পার্থক্য কি ?
+
+// যদি তুমি শুধু জানতে চাও কোনো item array-তে আছে কিনা → ✅ includes()
+// যদি item টা কোন index-এ আছে সেটা জানতে চাও → ✅ indexOf()
+
+
+
+
+

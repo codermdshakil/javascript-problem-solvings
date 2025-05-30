14c752e4b8c111a2ebc3914414df60ff32e29c70
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-05-30 23:02:25 +0600

    Problem solved: object - 1

---
+++ 28-object.js
@@ -0,0 +1,13 @@
+
+// 1 
+
+const teacher = {
+    name:"Shakil Ahmed",
+    subject:"Science",
+    age:21,
+    address:{
+        city:"Dhaka",
+        steet:"Barigong",
+        road:"Kapasia road",
+    }
+}

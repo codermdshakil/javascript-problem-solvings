ea324c30addc2415b4b9689481c40f94d04b8522
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-05 21:49:54 +0600

    Problme solved: Object Properties - 1

---
+++ 29-object-properties.js
@@ -0,0 +1,13 @@
+
+
+// JavaScript Object Propeties Practice problem solved
+
+// 1 
+const player = {
+    name:"Sakib Al Hasan",
+    age:36,
+    sports:"Cricket",
+    team:"BangladeshTeam"
+};
+
+console.log(player.team);

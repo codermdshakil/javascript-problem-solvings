c3370f9a14f3ace9e35b6352534785bd55924c2b
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-06 23:23:44 +0600

    Problem sovled: Object - 7

--- 29-object-properties.js
+++ 29-object-properties.js
@@ -64,6 +64,15 @@ const movie = {
 
 // console.log(movie['rating']);
 
+// 7
+const college ={
+    name:"NDC",
+    established:1949,
+    groups:['Science', 'Arts', 'Commerce']
+};
+
+// console.log(college.groups[1]);
+
 
 
 

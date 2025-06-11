f5ec2d1449dca74823c68dd85b08f6dc5602d46e
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-11 11:58:25 +0600

    Problem solved: Object Protection - 2

--- 31-object-protection.js
+++ 31-object-protection.js
@@ -1,8 +1,4 @@
-
 //JavaScript Object Protection Problems
-
-// ২. নিচের অবজেক্টটি দাও: const player = { name: "Messi", goals: 800, club: "Inter Miami" };
-// Object.freeze() মেথড ব্যবহার করে অবজেক্টটি ফ্রিজ করো। এরপর একটি নতুন প্রপার্টি যোগ করে দেখো, কাজ করে কি না।
 
 // ৩. নিচের অবজেক্টটি দাও: const book = { title: "Harry Potter", author: "J.K. Rowling", pages: 500 };
 // Object.seal() ব্যবহার করে অবজেক্টটি সিল করো। তারপর author প্রপার্টিটি চেঞ্জ করে দেখো।
@@ -14,26 +10,24 @@
 // ৬. নিচের অবজেক্টটি দাও: const food = { name: "Pizza", price: 500, size: "Large" };
 // এমন কিছু করো, যাতে food অবজেক্টে নতুন কোনো প্রপার্টি যোগ করা না যায়, কিন্তু চাইলে price বাড়ানো বা কমানো যাবে।
 
-
 // Solutions:
 
 // ১. ধরো, একটি headphone অবজেক্ট আছে, যার মধ্যে রয়েছে brand: "Sony", price: 3000, এবং color: "red"। Object.freeze() মেথড ব্যবহার করে অবজেক্টটি ফ্রিজ করো। এরপর একটি নতুন প্রপার্টি যোগ করার চেষ্টা করো। দেখো, কাজ করে কি না।
 
-
 const headphone = {
-  brand:"Sony",
-  price:3000,
-  color:"Red"
+  brand: "Sony",
+  price: 3000,
+  color: "Red",
 };
 
 Object.freeze(headphone);
+headphone.brand = "Readland"; // Not allow
+// console.log(headphone)
 
-headphone.brand = "Readland";
-console.log(headphone)  
+// ২. নিচের অবজেক্টটি দাও: const player = { name: "Messi", goals: 800, club: "Inter Miami" };
+// Object.freeze() মেথড ব্যবহার করে অবজেক্টটি ফ্রিজ করো। এরপর একটি নতুন প্রপার্টি যোগ করে দেখো, কাজ করে কি না।
 
-
-
-
-
-
-
+const player = { name: "Messi", goals: 800, club: "Inter Miami" };
+Object.freeze(player);
+player.salary = 5000000;
+console.log(player);
